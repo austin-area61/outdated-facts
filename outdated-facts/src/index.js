7587f82114f1d4a9d1fa1de0c5959273b03aa6e2
austin-area61 <austinonyango.area61@gmail.com>
2024-10-01 12:38:18 +0300

    set up server to handle incoming requests

--- outdated-facts/src/index.js
+++ outdated-facts/src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+mongoose.connect('mongodb://localhost:27017/outdatedFacts', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const factSchema = new mongoose.Schema({
+  fact: String,
+  yearDisproved: Number
+});
+
+const Fact = mongoose.model('Fact', factSchema);
+
+// Seed database with some example facts
+app.get('/facts/:year', async (req, res) => {
+  const year = parseInt(req.params.year);
+  const facts = await Fact.find({ yearDisproved: { $gt: year } });
+  res.json(facts.map(fact => fact.fact));
+});
+
+app.listen(5000, () => {
+  console.log('Server is running on port 5000');
+});
